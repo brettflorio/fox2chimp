159e63831dc721dae8b2da5efc90dc40dd1806e2
fred <fred@augray.localdomain>
2008-07-21 11:16:21 -0700

    Plays nicely with PHP4.

--- MailChimpUtils.php
+++ MailChimpUtils.php
@@ -2,40 +2,39 @@
 require 'MCAPI.class.php';
 
 /**
- * Given a user, the name of a MailChimp list, and the MailChimp API credentials, subscribes user
- *  to named list.
+ * Given a user, the name of a MailChimp list, and the MailChimp API credentials,
+ * subscribe user to named list. <b>Will die() on any errors from MailChimp.</b>
  *
  * For the MailChimp API documentation pertinent to subscribing a user, see here:
- *  <a href="http://www.mailchimp.com/api/1.0/listsubscribe.func.php">MailChimp API</a>
+ *  <a href="http://www.mailchimp.com/api/1.0/listsubscribe.func.php">MailChimp
+ *  API</a>
  * 
- * @param array $user           Contains the information about the user to subscribe.  Keys:
- *                                 'first_name'         => string; the user's first name
- *                                 'last_name'          => string; the user's last name
- *                                 'email'              => string; the user's email address
- *                                 'format'  (optional) => string; list format preference, either 'html' or 'text'
- *                                                         If not present, defaults to 'html'.
- *                                 'confirm' (optional) => boolean; when true, send a subscription confirmation.
- *                                                         If not present, defaults to true.
+ * @param array $user	Contains the information about the user to subscribe.  Keys:
+ *                     'first_name'         => string; the user's first name
+ *                     'last_name'          => string; the user's last name
+ *                     'email'              => string; the user's email address
+ *                     'format'  (optional) => string; list format preference,
+ *																							        either 'html' or 'text'
+ *                                             If not present, defaults to 'html'.
+ *                     'confirm' (optional) => boolean; when true, send a
+ *																							subscription confirmation.
+ *                                              If not present, defaults to true.
  *
  * @param string $list_name     The name of the list to subscribe to.
  *
- * @param array $credentials    Contains the credentials for the MailChimp account that owns the list.  Keys:
- *                                  'user'              => string; MailChimp username
- *                                  'pass'              => string; MailChimp password
- *
- * @throws  Exception           On any error while communicating with MailChimp.
+ * @param array $credentials    Contains the credentials for the MailChimp account
+ *															 that owns the list.  Keys:
+ *                               'user'              => string; MailChimp username
+ *                               'pass'              => string; MailChimp password
  *
  * @return  boolean             Returns true if member subscribed to the list.
  */
 function subscribe_user_to_list($user, $list_name, $credentials) {
     $mc = new MCAPI($credentials['user'], $credentials['pass']);
 
-    if ($mc->errorMessage) {
-        throw new Exception("Unable to connect to MailChimp API, error: ".
-         $mc->errorMessage);
-    }
+    $mc or die("Unable to connect to MailChimp API, error: ".$mc->errorMessage);
 
-    $lists = $mc->lists();  // Retrieve all lists from MailChimp.
+    $lists = $mc->lists() or die($mc->errorMessage);
     $list_id = null;
 
     foreach ($lists AS $list) { // Iterate, finding the list named $list_name
@@ -43,9 +42,8 @@ function subscribe_user_to_list($user, $list_name, $credentials) {
             $list_id = $list['id'];
         }
     }
-    
-    if (!$list_id)  // Did we find the list?
-        throw new Exception("Couldn't find a list named '$list_name'!");
+
+		$list_id or die("Couldn't find a list named '$list_name'!");
 
     $retval = $mc->listSubscribe($list_id,
      $user['email'],
@@ -54,14 +52,10 @@ function subscribe_user_to_list($user, $list_name, $credentials) {
      isset($user['confirm']) ? $user['confirm'] : true
      );
 
-    if (!$retval) { // Did we subscribe the user?
-        if (!preg_match("/already subscribed/i", $mc->errorMessage)) {
-            throw new Exception("Unable to load listSubscribe()! ".
-             "MailChimp reported error:\n".
-             "\tCode=".$mc->errorCode."\n".
-             "\tMsg=".$mc->errorMessage."\n");
-        }
-    }
+    ($retval or preg_match("/already subscribed/i", $mc->errorMessage)) or 
+		 die("Unable to load listSubscribe()! ".
+     "MailChimp reported error:\n\tCode=".$mc->errorCode.
+     "\n\tMsg=".$mc->errorMessage."\n");
 
     return true;    // All's well.
 }
--- fc-mailchimp.php
+++ fc-mailchimp.php
@@ -24,39 +24,41 @@ $key = 'CHANGE THIS TEXT to your own datafeed keyphrase';
 
 $_POST['FoxyData'] or die("error"); // Make sure we got passed some FoxyData
 
-try {
-    $FoxyData = rc4crypt::decrypt($key, urldecode($_POST["FoxyData"]));
-
-    $data = new XMLParser($FoxyData);   // Parse that XML.
-    $data->Parse();
-    
-    foreach ($data->document->transactions[0]->transaction as $tx) {
-        $subscribe = !$Use_Custom_Field;
-
-        if ($Use_Custom_Field) {
-            foreach ($tx->custom_fields[0]->custom_field as $field) {
-                $subscribe = $subscribe ||
-                 ($field->custom_field_name[0]->tagData == $Custom_Field &&
-                  $field->custom_field_value[0]->tagData == $Custom_Field_Value);
-            }
-        }
-
-        if ($subscribe) {
-            subscribe_user_to_list(// See MailChimpUtils.php for documentation.
-             array( 
-              'first_name' => $tx->customer_first_name[0]->tagData,
-              'last_name' => $tx->customer_last_name[0]->tagData,
-              'email' => $tx->customer_email[0]->tagData,
-              'format' => $Email_Format,
-              'confirm' => $Send_Confirmation),
-             'My First List',
-             $MailChimp_Auth);
-        }
-    }
-    print "foxy";
+function fatal_error_handler($errno, $errstr, $errfile, $errline, $errcontext) {
+	die($errstr);
+	return true;
 }
-catch (Exception $e) {
-    print "error\n\n";
-    print $e;   // Nothing else that we can do.  Hopefuly someone will notice.
+set_error_handler(fatal_error_handler);
+
+$FoxyData = rc4crypt::decrypt($key, urldecode($_POST["FoxyData"]));
+
+$data = new XMLParser($FoxyData);   // Parse that XML.
+$data->Parse();
+
+foreach ($data->document->transactions[0]->transaction as $tx) {
+				$subscribe = !$Use_Custom_Field;
+
+				if ($Use_Custom_Field) {
+						foreach ($tx->custom_fields[0]->custom_field as $field) {
+								$subscribe = $subscribe ||
+								 ($field->custom_field_name[0]->tagData == $Custom_Field &&
+									$field->custom_field_value[0]->tagData == $Custom_Field_Value);
+						}
+				}
+
+				if ($subscribe) {
+						subscribe_user_to_list(// See MailChimpUtils.php for documentation.
+						 array( 
+							'first_name' => $tx->customer_first_name[0]->tagData,
+							'last_name' => $tx->customer_last_name[0]->tagData,
+							'email' => $tx->customer_email[0]->tagData,
+							'format' => $Email_Format,
+							'confirm' => $Send_Confirmation),
+						 'My First List',
+						 $MailChimp_Auth);
+				}
 }
+
+print "foxy";
+
 ?>
